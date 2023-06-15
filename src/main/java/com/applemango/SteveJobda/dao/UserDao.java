e95dd176b9a6b100441208083edaa890324e465d
jsw0824 <jsw0824@midasin.com>
2023-06-15 17:49:31 +0900

    refactor: service, dao 정리

--- src/main/java/com/applemango/SteveJobda/dao/UserDao.java
+++ src/main/java/com/applemango/SteveJobda/dao/UserDao.java
@@ -1,6 +1,5 @@
 package com.applemango.SteveJobda.dao;
 
-import com.applemango.SteveJobda.controller.rqrs.UserDetailRs;
 import com.applemango.SteveJobda.db.generate.dao.UserMapper;
 import com.applemango.SteveJobda.db.generate.model.User;
 import com.applemango.SteveJobda.db.generate.model.UserExample;
@@ -44,7 +43,15 @@ public class UserDao implements BaseDao<User> {
         return userMapper.selectByPrimaryKey(key);
     }
 
+    /**
+     * 아이디로 유저 조회
+     *
+     * @param id
+     * @return 아이디가 일치하는 유저
+     * @설명 아이디가 일치하는 유저 조회
+     */
     public User findById(String id) {
+
         UserExample example = new UserExample();
         UserExample.Criteria criteria = example.createCriteria();
         criteria.andIdEqualTo(id);
@@ -56,8 +63,15 @@ public class UserDao implements BaseDao<User> {
         return users.get(0);
     }
 
-    /* 탈퇴한 계정도 검사 */
+    /**
+     * 아이디로 유저가 있는지 조회
+     *
+     * @param id
+     * @return 아이디 존재여부
+     * @설명 아이디 존재여부 검사 (탈퇴한 계정도 아이디 중복 비허용)
+     */
     public boolean isExistById(String id) {
+
         UserExample example = new UserExample();
         UserExample.Criteria criteria = example.createCriteria();
         criteria.andIdEqualTo(id);
@@ -69,34 +83,25 @@ public class UserDao implements BaseDao<User> {
         return true;
     }
 
-    /* 로그인 가능 여부를 알기 위함(탈퇴하지 않은 계정만) */
+    /**
+     * 아이디와 패스워드로 탈퇴하지 않은 계정이 있는지
+     *
+     * @param id
+     * @param password
+     * @return 아이디와 비밀번호가 일치하며 탈퇴하지 않은 계정 존재여부
+     * @설명 로그인 가능 여부 검사
+     */
     public boolean isExistByIdAndPassword(String id, String password) {
+
         UserExample example = new UserExample();
         UserExample.Criteria criteria = example.createCriteria();
-        criteria.andIdEqualTo(id).andPasswordEqualTo(password);
+        criteria.andIdEqualTo(id).andPasswordEqualTo(password).andDeleteYnEqualTo(false);
         List<User> users = userMapper.selectByExample(example);
 
-        /* 조회되지 않는 경우 */
         if (users.size() == 0) {
             return false;
         }
-        User user = users.get(0);
-        /* 조회되었으나 탈퇴한 계정인 경우 */
-        if (user.getDeleteYn()) {
-            return false;
-        }
-        /* 로그인 가능한 계정 (조회되며 탈퇴하지 않은 계정) */
         return true;
-    }
-
-    public UserDetailRs test(Integer user_sn) {
-        User user = userMapper.selectByPrimaryKey(user_sn);
-        return UserDetailRs.builder()
-                .id(user.getId())
-                .name(user.getName())
-                .team(user.getTeam())
-                .phone(user.getPhone())
-                .build();
     }
 
 }
